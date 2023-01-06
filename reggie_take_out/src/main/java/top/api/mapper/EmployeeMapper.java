9ad15aa276c3d596c864198dad98ded06e50f275
yql <2746576130@qq.com>
2023-01-06 13:48:56 +0800

    'updata'

--- reggie_take_out/src/main/java/top/api/mapper/EmployeeMapper.java
+++ reggie_take_out/src/main/java/top/api/mapper/EmployeeMapper.java
@@ -3,11 +3,11 @@ package top.api.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import top.api.pojo.Employee;
-import org.apache.ibatis.annotations.*;
 
 /**
  * 员工实体类
  */
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
+    void add();
 }
